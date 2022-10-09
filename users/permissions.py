c57dd21cfcb6a29e85cf3c88c9eba5f52e9b625d
Ivan Goryachev <23.gorjachev.i@gmail.com>
2022-10-09 18:10:26 +0300

    Added authorization in api and some permissions
    1. UserList is available for everyone
    2. Changing superuser or staff account is only available for superuser
    3. Changing any other account is available for staff, superuser and account owner

---
+++ users/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+
+class IsAccountOwnerOrReadOnly(permissions.BasePermission):
+    # Also if user is_staff it means that he "is account owner"
+    def has_object_permission(self, request, view, obj):
+        # Read-only permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
+            # SAFE_METHODS are available for everyone
+            # superuser can do whatever he wants hehe
+            return True
+        if request.user.is_staff and not obj.is_superuser and not obj.is_staff:
+            # staff can do whatever they want to any accs except superuser and staff ones
+            return True
+        return obj == request.user
--- users/views.py
+++ users/views.py
@@ -1,13 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import CustomUser
 from .serializers import UserSerializer
 
+from .permissions import IsAccountOwnerOrReadOnly
+
 
 class UserList(generics.ListAPIView):
+    permission_classes = (permissions.AllowAny,)
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAccountOwnerOrReadOnly,)
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
