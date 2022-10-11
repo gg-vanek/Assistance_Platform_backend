992cc7ca4d42593ee435ef82738cdb16570da3f2
Ivan Goryachev <23.gorjachev.i@gmail.com>
2022-10-11 22:17:58 +0300

    Added refresh token rotation
    That means that after using refresh_token_1 you get new access_token and refresh_token_2 (and refresh_token_1 becomes not valid anymore)

--- assistance_platform_project/settings.py
+++ assistance_platform_project/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
 
