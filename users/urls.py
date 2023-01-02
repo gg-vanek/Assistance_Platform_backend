b12eab064292fb30a91b9dd4089df1a5327a1e29
Ivan Goryachev <23.gorjachev.i@gmail.com>
2023-01-03 00:44:54 +0300

    Добавил получение профиля юзера по юзернейму

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from .views import UserList, UserDetail
 
 urlpatterns = [
     path('<int:pk>', UserDetail.as_view()),
+    path('<str:username>', UserDetail.as_view(lookup_field='username')),
     path('', UserList.as_view()),
 ]
