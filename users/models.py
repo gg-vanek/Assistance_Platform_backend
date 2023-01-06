d317e64c23fe37e313348c86fef5b122d63d8642
Ivan Goryachev <23.gorjachev.i@gmail.com>
2023-01-06 21:18:25 +0300

    Добавил проверку на неравенство username системным словам. Добавил проверку на уникальность email

--- users/models.py
+++ users/models.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
 import os
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.functional import lazy
+from django.utils.translation import gettext
 
 STAGE_OF_STUDY_CHOICES = [
     ('N', 'None'),
@@ -13,10 +17,33 @@ STAGE_OF_STUDY_CHOICES = [
     ('PG', "postgraduate study"),
 ]
 
+gettext_lazy = lazy(gettext, str)
+
+RESERVED_SYSTEM_WORDS = ['my_profile', 'registration', 'id']
+
+
+def not_reserved_system_word_validator(username):
+    if username in RESERVED_SYSTEM_WORDS:
+        raise ValidationError(gettext_lazy('%(username)s is a reserved system word. Choose another username'),
+                              params={'value': username}, )
+
 
 class User(AbstractUser):
-    # TODO добавить уникальность эмейла при сохранении
-    # добавить проверку на неравенство username 'my_profile', 'registration'
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        gettext_lazy("username"),
+        max_length=150,
+        unique=True,
+        help_text=gettext_lazy(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator, not_reserved_system_word_validator],
+        error_messages={
+            "unique": gettext_lazy("A user with that username already exists."),
+        },
+    )
+    email = models.EmailField(gettext_lazy("email address"), blank=True, unique=True)
+
     biography = models.TextField(blank=True)
 
     profile_image = models.ImageField(null=True, blank=True,
@@ -38,6 +65,5 @@ class User(AbstractUser):
     doer_rating_sum = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     doer_rating_count = models.IntegerField(default=0, validators=[MinValueValidator(0)])
 
-    # TODO добавить больше контактов
     def __str__(self):
         return self.username
