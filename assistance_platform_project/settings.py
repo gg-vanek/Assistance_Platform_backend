9e368be6fe4a787022ee08a37eb1ffe9a90c7d23
Ivan Goryachev <23.gorjachev.i@gmail.com>
2023-01-07 06:47:07 +0300

    access token действует 1 час

--- assistance_platform_project/settings.py
+++ assistance_platform_project/settings.py
@@ -162,7 +162,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
