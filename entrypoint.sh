91101622790efc4ceba408a84805629a87d8fdf7
Ivan Goryachev <23.gorjachev.i@gmail.com>
2023-03-28 03:29:33 +0300

    Добавлено несколько фикстур заданий, имеющих презентабельный вид. Изменем Ентрипоинт

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,25 @@ done
 
 echo "Prepare database migrations"
 python manage.py makemigrations
-
 echo "Apply database migrations"
 python manage.py migrate
 
+
 echo "Creating superuser"
 python manage.py loaddata fixtures/superuser.json
-
 echo "Loading some default tags and subjects"
 python manage.py loaddata fixtures/task_tags_and_subjects.json
 
-echo "Loading some default data"
-python manage.py loaddata fixtures/some_data.json
+
+echo "Loading some site users"
+python manage.py loaddata fixtures/users.json
+echo "Loading some tasks"
+python manage.py loaddata fixtures/tasks.json
+echo "Loading some notifications"
+python manage.py loaddata fixtures/notifications.json
+echo "Loading some applications"
+python manage.py loaddata fixtures/applications.json
+
 
 echo "Runserver"
 python manage.py runserver 0.0.0.0:8000
